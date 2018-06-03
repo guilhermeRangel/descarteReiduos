274d59ce7101ef7fe622f61953361151722f1d70
guilhermeRangel <gui.vg16@hotmail.com>
2018-06-03 15:58:03 -0300

    adicionado formatador de Erros

--- myApi/ecoPontoService.js
+++ myApi/ecoPontoService.js
@@ -1,7 +1,28 @@
 const ecoPontos = require('./ecoPontos')
-
+const _ = require('lodash')
 ecoPontos.methods(['get', 'post', 'put', 'delete'])
 ecoPontos.updateOptions({new: true, runValidators: true}) //put retorna um novo obj
+
+
+
+function parseErrors(nodeRestFulErrors){
+    const errors = []
+    _.forIn(nodeRestFulErrors, error => errors.push(error.message))
+    return errors
+}
+
+
+ecoPontos.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res.locals.bundle //contem os erros 
+    if(bundle.errors) {
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
 
 ecoPontos.route('countObj', function(req, res, next) {
     ecoPontos.count(function(erro, valor){
