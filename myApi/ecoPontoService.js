40a67c6a59eb555fd18116f83ada91213daea5ed
guilhermeRangel <gui.vg16@hotmail.com>
2018-06-03 15:22:55 -0300

    implementado contador de objetos

--- myApi/ecoPontoService.js
+++ myApi/ecoPontoService.js
@@ -1,6 +1,17 @@
 const ecoPontos = require('./ecoPontos')
 
 ecoPontos.methods(['get', 'post', 'put', 'delete'])
-ecoPontos.updateOptions({new: true, runValidators: true}) //cria um novo obj e valida
+ecoPontos.updateOptions({new: true, runValidators: true}) //put retorna um novo obj
+
+ecoPontos.route('countObj', function(req, res, next) {
+    ecoPontos.count(function(erro, valor){
+        if(erro) {
+            res.status(500).json({errors: [erro]})
+        } else {
+            res.json({valor})
+        }
+    })
+})
+
 module.exports = ecoPontos
 
