dbc57e8a67cfccbddc872e6e93df4a2a2d79b736
guilhermeRangel <gui.vg16@hotmail.com>
2018-06-03 15:09:39 -0300

    testes em get,post, put e delete

--- configuracoes/banco.js
+++ configuracoes/banco.js
@@ -3,3 +3,5 @@ const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/de_ecoPontos') //conecta no banco se nao houver ele cria
 
 mongoose.Error.messages.general.required = "O atributi '{PATH}' é obrigatorio"
+mongoose.Error.messages.String = "'{VALUE}' não é valido para o atributo '{PATH}.'"
+mongoose.Error.messages.Number = "'{VALUE}' não é valido para o atributo '{PATH}.'"
--- myApi/ecoPontoService.js
+++ myApi/ecoPontoService.js
@@ -1,5 +1,6 @@
 const ecoPontos = require('./ecoPontos')
 
 ecoPontos.methods(['get', 'post', 'put', 'delete'])
-
+ecoPontos.updateOptions({new: true, runValidators: true}) //cria um novo obj e valida
 module.exports = ecoPontos
+
